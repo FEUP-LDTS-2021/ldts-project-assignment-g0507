cc5e03e012e5f7302c39e8bd93b783962be0674c
Andre Cruz <andr3.cruz96@gmail.com>
2022-01-20 01:10:36 +0000

    refactored Game and added LanternaGUI

--- src/main/java/com/Game.java
+++ src/main/java/com/Game.java
@@ -1,54 +1,46 @@
 package com;
 
-import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
+
+import com.gui.LanternaGUI;
 
 import java.io.IOException;
 
 public class Game {
-
-    Screen screen;
+    private final LanternaGUI GUI;
     Paddle paddle;
 
     public Game() throws IOException {
+        GUI = new LanternaGUI();
+        paddle = new Paddle(35,35);
+    }
 
-        try {
-            DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(80,40));
-            Terminal terminal = defaultTerminalFactory.createTerminal();
-            screen = new TerminalScreen(terminal);
-
-            screen.setCursorPosition(null);
-            screen.startScreen();
-
-            paddle = new Paddle(35,35);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
     }
 
     private void draw() throws IOException {
-        screen.clear();
-        paddle.draw(screen.newTextGraphics());
-        screen.refresh();
+        paddle.draw(GUI.getScreen().newTextGraphics());
+        GUI.refresh();
     }
 
     public void run() throws IOException {
         boolean running = true;
         draw();
         while (running) {
-            KeyStroke key = screen.readInput();
-            System.out.println(key.getKeyType());
-            if((key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') || (key.getKeyType() == KeyType.EOF)) running = false;
-            if(key.getCharacter() == 'a') movePaddle(paddle.moveLeft());
-            if(key.getCharacter() == 'd') movePaddle(paddle.moveRight());
+            KeyStroke key = GUI.getScreen().readInput();
+            if(key.getCharacter() != null) {
+                System.out.println(key.getKeyType() + " " + key.getCharacter());
+                if ((key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') || (key.getKeyType() == KeyType.EOF))
+                    running = false;
+                if (key.getCharacter() == 'a') movePaddle(paddle.moveLeft());
+                if (key.getCharacter() == 'd') movePaddle(paddle.moveRight());
+            }
             draw();
         }
-        screen.close();
+        GUI.close();
     }
 
     private void movePaddle(Position position) {
---
+++ src/main/java/com/gui/LanternaGUI.java
@@ -0,0 +1,46 @@
+package com.gui;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class LanternaGUI {
+    private final Screen screen;
+
+    public LanternaGUI() throws IOException {
+        Terminal terminal = createTerminal();
+        screen = createScreen(terminal);
+    }
+
+    public Screen getScreen(){
+        return screen;
+    }
+
+    private Terminal createTerminal() throws IOException {
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(80,40));
+        return defaultTerminalFactory.createTerminal();
+    }
+
+    private Screen createScreen(Terminal terminal) throws IOException {
+        final Screen screen;
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        return screen;
+    }
+
+    public void refresh() throws IOException {
+        screen.refresh();
+        screen.clear();
+    }
+
+    public void close() throws IOException {
+        screen.close();
+    }
+}
