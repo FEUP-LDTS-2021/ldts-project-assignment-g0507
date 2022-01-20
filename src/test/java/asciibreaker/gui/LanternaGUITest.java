06ac7fafe86cb98100a9d4794e0c99ec97ccfaa9
Andre Cruz <andr3.cruz96@gmail.com>
2022-01-20 14:27:12 +0000

    Added tests for testDrawButton method to implement in LanternaGUI

---
+++ src/test/java/asciibreaker/gui/LanternaGUITest.java
@@ -0,0 +1,33 @@
+package asciibreaker.gui;
+
+import asciibreaker.model.Button;
+import asciibreaker.model.Position;
+import com.googlecode.lanterna.TextColor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class LanternaGUITest {
+
+    private LanternaGUI lanternaGUI;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        lanternaGUI = new LanternaGUI();
+    }
+
+    @Test
+    void TestDrawButton() {
+        Button button = new Button("test",
+                new Position(10,10),
+                new Position(20,20),
+                "#0FFFFF",
+                "FFFFFF");
+
+        lanternaGUI.drawButton(button);
+        Assertions.assertEquals(" ", lanternaGUI.getScreen().getBackCharacter(10, 10).getCharacterString());
+        Assertions.assertEquals(TextColor.Factory.fromString("#0FFFFF"), lanternaGUI.getScreen().getBackCharacter(10,10).getBackgroundColor());
+    }
+}
